24ac21cd502a11d796e2237975a120c01e3cd43c
0xvpr <malikrb@live.com>
2021-11-20 06:03:13 -0600

    added c format; staged cpp format; improved display functions

--- main.py
+++ main.py
@@ -12,17 +12,58 @@ Brief:
 """
 
 import requests
-# import argparse
+import sys
 
 from bs4 import BeautifulSoup
+from argparse import ArgumentParser
 
 from modules.scraper import parse_table
-from modules.formatter import display_as_python_tuple
+from modules.formatter import display_as_python_tuple, display_as_c_int_array
+
+from typing import Union, NoReturn
 
 source = "https://www.worldatlas.com/na/us/area-codes.html"
 
-def handle_command_line():
+def handle_command_line() -> Union[NoReturn, None, str]:
+    argc = len(sys.argv)
+
+    if argc > 2:
+        __usage_error(1) # only specify one switch pls
+
+    parser = ArgumentParser()
+
+    parser.add_argument( "-c",
+                         action="store_true",
+                         dest="c",
+                         help="Convert bytes in file to raw string." )
+
+    parser.add_argument( "--cpp",
+                         action="store_true",
+                         dest="cpp",
+                         help="Convert bytes in file to cstr." )
+
+    parser.add_argument( "--py",
+                         action="store_true",
+                         dest="py",
+                         help="Convert bytes in file to cstr." )
+
+    args = parser.parse_args()
+
+    if args.c == True:
+        return "c"
+    elif args.cpp == True:
+        return "cpp"
+    elif args.py == True:
+        return "py"
+
     return None
+
+def __usage_error(exit_code: int) -> NoReturn:
+    if exit_code == 1:
+        sys.stderr.write(f"{sys.argv[0]} [ -c | -cpp | -py ]\n\n")
+
+    sys.stderr.write(f"Exited with exit code: {exit_code}.")
+    sys.exit(exit_code)
 
 if __name__ == "__main__":
     # Determine output language format
@@ -41,9 +82,10 @@ if __name__ == "__main__":
     # Format based on user input
     if fmt is None:
         display_as_python_tuple(area_codes_map)
+    elif fmt == "c":
+        display_as_c_int_array(area_codes_map)
+    elif fmt == "cpp":
+        # TODO
+        display_as_c_int_array(area_codes_map)
     elif fmt == "py":
         display_as_python_tuple(area_codes_map)
-    elif fmt == "cpp":
-        ...
-    elif fmt == "c":
-        ...
--- modules/formatter.py
+++ modules/formatter.py
@@ -1,15 +1,16 @@
 def display_as_python_tuple(area_codes_map: dict) -> None:
-    variable_name = "us_area_codes_tuple = ( "
+    variable_name  = "us_area_codes_tuple = ( "
     spacing = " " * len(variable_name)
+    comment_prefix = "#"
 
     # print initial variable_name
     print(variable_name, end="")
     for i, (state, area_codes_list) in enumerate(area_codes_map.items()):
         # Pr
         if i == 0:
-            print(f"# {state}\n{spacing}", end="")
+            print(f"{comment_prefix} {state}\n{spacing}", end="")
         else:
-            print(f"{spacing}# {state}\n{spacing}", end="")
+            print(f"{spacing}{comment_prefix} {state}\n{spacing}", end="")
         
         breakline = 10
         for j, area_code  in enumerate(area_codes_list):
@@ -30,6 +31,49 @@ def display_as_python_tuple(area_codes_map: dict) -> None:
             # If element is final element in map, print ending parenthesis
             if i == len(area_codes_map)-1 and j == len(area_codes_list)-1:
                 print(" )")
+            # If element is final element in list, print newline
+            elif j == len(area_codes_list)-1:
+                print("")
+
+def display_as_c_int_array(area_codes_map: dict) -> None:
+    n_area_codes = 0
+    for _, area_codes in area_codes_map.items():
+        if isinstance(area_codes, tuple):
+            n_area_codes += len(area_codes)
+
+    variable_name = f"const int us_area_codes[{n_area_codes}] = {'{'} "
+    spacing = " " * len(variable_name)
+    comment_prefix = "//"
+
+
+    # print initial variable_name
+    print(variable_name, end="")
+    for i, (state, area_codes_list) in enumerate(area_codes_map.items()):
+        # Pr
+        if i == 0:
+            print(f"{comment_prefix} {state}\n{spacing}", end="")
+        else:
+            print(f"{spacing}{comment_prefix} {state}\n{spacing}", end="")
+        
+        breakline = 10
+        for j, area_code  in enumerate(area_codes_list):
+            # Separate elements by comma unless it is the last
+            # element in the map itself
+            if i < len(area_codes_map)-1:
+                print(f"{area_code}", end=", ")
+            else:
+                print(f"{area_code}", end="")
+
+            # Split elements by lines of 10 elements
+            if breakline == 1 and j != len(area_codes_list)-1:
+                print(f"\n{spacing}", end="")
+                breakline = 10
+            else:
+                breakline -= 1
+
+            # If element is final element in map, print ending parenthesis
+            if i == len(area_codes_map)-1 and j == len(area_codes_list)-1:
+                print(" };")
             # If element is final element in list, print newline
             elif j == len(area_codes_list)-1:
                 print("")
